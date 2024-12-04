db17788b69a3b17bf4fcb9e05a5b1f4a3de6df86
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-12-04 18:06:35 +0800

    1

--- b1.js
+++ b1.js
@@ -1,28 +1,151 @@
 //ondor jil
-//const year = prompt()
-//if (year%400===0) {
-//    console.log("ondor jil")
-//}else if (year%100===0) {
-//    console.log("Ondor jil bish")
-//}else if (year%4===0) {
-//    console.log("ondor jil")
-//}else{
-//    console.log("ondor jil bish")
-//}
+// const year = 2005
+// if (year%400===0) {
+//     console.log("ondor jil")
+// }else if (year%100===0) {
+//     console.log("Ondor jil bish")
+// }else if (year%4===0) {
+//     console.log("ondor jil")
+// }else{
+//     console.log("ondor jil bish")
+// }
 
-//dun
+// dun
+// const name ="Bat"
+// let dun = 90 ;
+// if (dun <=100 && dun >= 90) {
+//     console.log( name,"-", dun, "- Маш сайн")
+// }else if (dun<90 && dun>79) {
+//     console.log(name,"-",dun,"- сайн")
+// }else if (dun<80 && dun>69) {
+//     console.log(name,"-",dun,"- дунд")
+// }else if (dun<70 && dun>59) {
+//     console.log(name,"-",dun,"- хангалттай")
+// }else{
+//     console.log(name,"-",dun,"- маш муу")
+// }
 
-//const dun = prompt();
-//if (dun <=100 && dun >= 90) {
-//    console.log("Маш сайн")
-//}else if (dun<90 && dun>79) {
-//    console.log("сайн")
-//}else if (dun<80 && dun>69) {
-//    console.log("Дунд")
-//}else if (dun<70 && dun>59) {
-//    console.log("Хангалттай")
-//}else{
-//    console.log("Маш муу")
-//}
+//4
+// const num1=prompt() ,num2=prompt();
+// if ( num1>num2) {
+//     console.log(num1)
+// }else if ( num1<num2) {
+//     console.log(num2)
+// }else{
+//     console.log("tentsuu")
+// }
 
-console.log(1+"1"-1)
+//5
+// const phoneNumber = 99001234;
+// const opr= phoneNumber/1000000-phoneNumber/1000000%1
+// if (opr===99 || opr===94 || opr===85) {
+//     console.log("mobicom")
+// }else if (opr===80 || opr===86 || opr===88) {
+//     console.log("unitel")
+// }else if (opr===98 || opr===97 || opr===93) {
+//     console.log("G-mobile")
+// }else if (opr===90 || opr===91) {
+//     console.log("Skytel")
+// }
+
+//6
+// const suuri1 = 12;
+// const suuri2 = 5;
+// const gipt = (suuri2**2+suuri1**2)**(1/2);
+// console.log(gipt)
+
+//7
+// const a=85, b=75, c=96, d=69;
+// if (a>=80 && b>=80 && c>=80 && d>=80) {
+//     console.log(a+b+c+d)
+// }else if ()
+
+//8
+
+//9
+// const myAge = prompt("Nasaa oruulna uu:") ;
+// const yourAge = prompt("Nasaa oruulna uu:");
+// if ( myAge>yourAge) {
+//     console.log("Чи")
+// }else if (myAge<yourAge) {
+//     console.log("Та")
+// }
+
+//10
+// const x1 = 120, x2 =33 , x3 = 10, x4 =12, x5=40;
+// if (x1<x2 && x1<x3 && x1<x4 && x1<x5){
+//     console.log(x1)
+// }else if (x2<x3 && x2<x4 && x2<x5){
+//     console.log(x2)
+// }else if (x3<x4 && x3<x5){
+//     console.log(x3)
+// }else if (x4<x5){
+//     console.log(x4)
+// }else {
+//     console.log(x5)
+// }
+
+//11
+// const too1 = prompt();
+// if (too1%21===0) {
+//     console.log("3, 7-д хуваагддаг")
+// }else if(too1%7===0) {
+//     console.log("7d huvaagddag")
+// }else if (too1%3===0) {
+//      console.log("3-d huvaagddag")
+// }else{
+//     console.log("3 ba 7-d huvaagddagui")
+// }
+
+//12
+// const n = prompt();
+// if (n<0){
+//     console.log("negative")
+// }else if (n>0) {
+//      console.log("positive")
+// }else if (n===0) {
+//      console.log("n=0");    
+// }else{
+//     console.log("too oruulna uu")
+// }
+
+//13
+// const weigth = prompt("weight");
+// const heigth = prompt("height:");
+// const index = weigth/(heigth**2);
+// if (index>=95) {
+//      console.log("Overweight");
+// }else if (index<=95 && index>=85) {
+//      console.log("At risk of iver weight")
+// }else if (index>=5 && index<85) {
+//     console.log("Healthy weight")
+// }else if (index<5){
+//     console.log("Underweigth")
+// }
+
+//14
+// const number7 = prompt();
+// if (number7%2===0) {
+//     console.log("odd number")
+// }else{
+//     console.log("Even number")
+// }
+
+//15
+// const yearOfBirth = prompt("born:");
+// const d = new Date();
+// const year=d.getFullYear();
+// const old = year-yearOfBirth;
+// if (old>=0 && old<=1) {
+//     console.log("Infant")
+// }else if (1<old<=3) {
+//     console.log("Toddler")
+// }else if (3<old<=5) {
+//     console.log("Preschoool")
+// }else if (5<old<=12) {
+//     console.log("Gradeschool")
+// }else if (12<old<=18) {
+//     console.log("Teen")
+// }else if (18<old<=21) {
+//     console.log("Youngadult")
+// }
---
+++ b2.js
@@ -0,0 +1,53 @@
+// const num = prompt();
+// if (num>0) {
+//     console.log(num,"is positive")
+// }else if (num<0) {
+//     console.log(num,"is negative")
+// }else if (num==0) {
+//     console.log(num,"is zero")
+// }
+
+//2
+// const temp = prompt();
+// if (temp>30){
+//     console.log("Above")
+// }else if (0<=temp<=30) {
+//     console.log("normal")
+// }else if (temp<0) {
+//     console.log("Below")
+// }
+
+//3
+// const char = prompt();
+// if (char==="a" || char==="e" ||char==="u" || char==="i"  || char==="o") {
+//     console.log(char, "is a vowel")
+// }else{
+//     console.log(char, "is consonant")
+// }
+
+//8
+// const num3 = prompt();
+// if (num3/100>1){
+//     console.log(num3, "is a triple-digit number")
+// }else if (num3/10>1){
+//     console.log(num3, "is a double-digit number")
+// }else if (num3/1>1){
+//     console.log(num3, "is a single digit number")
+// }
+
+//9
+// let letter = prompt()
+// if (letter===letter.toLowerCase()){
+//     console.log(letter, "is lowerCase")
+// }else{
+//     console.log(letter, "is Upper case")
+// }
+
+//10
+let day = prompt();
+
+if (1<=day1<=5){
+    console.log("weekday")
+}else if (6<=day1<=7){
+    console.log("Weekend")
+}
